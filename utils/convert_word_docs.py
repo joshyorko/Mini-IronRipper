0de182e625fe9dfe2c429b57d91893b94bc53299
kdlocpanda <joshua.yorko@gmail.com>
2023-08-20 12:15:32 -0400

    Refactor code and remove unused files
    Refactor pdf_ocr.py
    Move utility functions to utils module
    Import required utils instead of having everything in main script
    Remove database code
    Call utils methods for file processing and output writing
    Reduce script length and complexity
    Delete unused utility scripts for image and Word conversion
    Remove temporary notebook used for testing
    This commit continues refactoring the code by moving functionality to utils and cleaning up the main script. Unused utility scripts are removed along with a temporary notebook. The changes improve code organization and readability.

--- utils/convert_word_docs.py
+++
@@ -1,7 +0,0 @@
-import os
-import subprocess
-
-def convert_doc_to_pdf(doc_file):
-    pdf_file = f"{os.path.splitext(doc_file)[0]}.pdf"
-    subprocess.run(["libreoffice", "--headless", "--convert-to", "pdf", doc_file, "--outdir", os.path.dirname(doc_file)])
-
